7afbd40356f13a8118b6c866b0aadfed857eb26e
zeIbdo <ibrahimrm@code.edu.az>
2024-05-20 01:53:04 +0400

    showing specific currency related info was completed

--- currencyTask/Program.cs
+++ currencyTask/Program.cs
@@ -8,8 +8,12 @@ namespace currencyTask
             string[] currencies = { "USD", "EUR", "JPY", "TRY", "GBP" };
             decimal[] rates = { 1.7M, 1.8455M, 1.0911M, 0.0527M, 2.1513M };
             int idx = 0;
+            int specificIdx = 0;
+            bool ent = false;
             bool env = true;
+            string specificCurrency;
             Console.WriteLine("Do you want to see all currencies and their rate for AZN?(if you want type \"show-recent-currency-rates\")");
+            Console.WriteLine("Do you want to see specific currency and its rate for AZN?(if you want type \"find-currency-rate-by-code\")");
             while (env == true)
             {
                 string answer = Console.ReadLine();
@@ -20,6 +24,29 @@ namespace currencyTask
                         Console.WriteLine($"{currencies[idx]}: {rates[idx]}");
                         idx++;
                     }
+                    idx = 0;
+                    Console.WriteLine("if you want to exit type \"exit\"");
+                }
+                else if (answer == "find-currency-rate-by-code")
+                {
+                    Console.WriteLine("If you want to see specific currency rate type its alpha code");
+                    specificCurrency = Console.ReadLine();
+                    while (idx < currencies.Length)
+                    {
+                        if (currencies[idx] == specificCurrency)
+                        {
+                            ent = true;
+                            specificIdx = idx;
+                            break;
+                        }
+                        idx++;
+                    }
+                    if (ent == false)
+                    {
+                        Console.WriteLine("Your alpha code which you wrote wasnt found in database");
+                    }
+                    else
+                        Console.WriteLine($"{currencies[specificIdx]}: {rates[specificIdx]}");
                     idx = 0;
                     Console.WriteLine("if you want to exit type \"exit\"");
                 }
