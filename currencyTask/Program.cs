1cc2584cd48ff131758c68d694a6afd006b3eda6
zeIbdo <ibrahimrm@code.edu.az>
2024-05-20 01:59:27 +0400

    currency rate based calculation was completed

--- currencyTask/Program.cs
+++ currencyTask/Program.cs
@@ -11,9 +11,12 @@ namespace currencyTask
             int specificIdx = 0;
             bool ent = false;
             bool env = true;
+            decimal conversionTotal = 0;
+            decimal conversionAmount = 0;
             string specificCurrency;
             Console.WriteLine("Do you want to see all currencies and their rate for AZN?(if you want type \"show-recent-currency-rates\")");
             Console.WriteLine("Do you want to see specific currency and its rate for AZN?(if you want type \"find-currency-rate-by-code\")");
+            Console.WriteLine("Do you want to convert your currency to AZN?(if you want type \"calculate-amount-by-currency\")");
             while (env == true)
             {
                 string answer = Console.ReadLine();
@@ -49,6 +52,31 @@ namespace currencyTask
                         Console.WriteLine($"{currencies[specificIdx]}: {rates[specificIdx]}");
                     idx = 0;
                     Console.WriteLine("if you want to exit type \"exit\"");
+                }
+                else if (answer == "calculate-amount-by-currency")
+                {
+                    Console.WriteLine("Type amount and alpha code of your currency one by one: ");
+                    Console.Write("Amount: ");
+                    conversionAmount = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine("Alpha code: ");
+                    specificCurrency = Console.ReadLine();
+                    while (idx < currencies.Length)
+                    {
+                        if (currencies[idx] == specificCurrency)
+                        {
+                            ent = true;
+                            specificIdx = idx;
+                            conversionTotal = rates[specificIdx] * conversionAmount;
+                            break;
+                        }
+                        idx++;
+                    }
+                    if (ent == false)
+                        Console.WriteLine("Your alpha code which you wrote wasnt found in database");
+                    else
+                        Console.WriteLine($"{currencies[specificIdx]}: {conversionTotal}");
+                    Console.WriteLine("if you want to exit type \"exit\"");
+                    idx = 0;
                 }
                 else if (answer == "exit")
                 {
